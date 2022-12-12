7694e5233dc8b4105bc17bbfa8cbe058f8b4dbb5
jasonkoehn <93397575+jasonkoehn@users.noreply.github.com>
2022-12-12 14:10:34 -0700

    Initial Commit

---
+++ ConvertTiles(Alpha)/ConvertTiles_Alpha_App.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertTiles_Alpha_App.swift
+//  ConvertTiles(Alpha)
+//
+//  Created by Jason Koehn on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ConvertTiles_Alpha_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
